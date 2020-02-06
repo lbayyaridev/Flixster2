49e84c3da56730dfc4978f1ae74ac6b61866a804
Laith <lbayyari@ucsd.edu>
2020-02-05 22:06:29 -0800

    Initial Commit

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Laith M Bayyari on 2/5/20.
+//  Copyright © 2020 Laith M Bayyari. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
